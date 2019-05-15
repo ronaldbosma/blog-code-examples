c6f23594f4ab16883d4fe833b6db3ca2a866b3d9
Ronald Bosma <ronald.bosma@alfam.nl>
2019-05-15 17:26:05 +0200

    First implementation of scenario with names

---
+++ UsingPronounsInSpecFlowScenarios/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingPronounsInSpecFlowScenarios
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string Address { get; set; }
+
+        public static void MoveInTogether(Person p1, Person p2, string newAddress)
+        {
+            p1.Address = newAddress;
+            p2.Address = newAddress;
+        }
+    }
+}
