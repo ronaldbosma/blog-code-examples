e0e2a784e909c6cbaceba6b72add23121a7c3264
Ronald Bosma <ronald.bosma@alfam.nl>
2019-05-15 17:02:43 +0200

    Initial solution for using pronouns in specflow scenario's

---
+++ UsingPronounsInSpecFlowScenarios/Persons.feature.cs
@@ -0,0 +1,3 @@
+﻿#error Version conflict - SpecFlow Visual Studio extension attempted to use SpecFlow code-behind generator 1.9, but project 'UsingPronounsInSpecFlowScenarios' references SpecFlow 3.0.
+#error We recommend migrating to MSBuild code-behind generation to resolve this issue.
+#error For more information see https://specflow.org/documentation/Generate-Tests-from-MsBuild/
---
+++ UsingPronounsInSpecFlowScenarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UsingPronounsInSpecFlowScenarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsingPronounsInSpecFlowScenarios")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74864154-e02b-47cc-b099-8f033ff787e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
