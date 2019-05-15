d65a28ab0e22fbc9ecbd79a4fb76b164fc152b1b
Ronald Bosma <ronald.bosma@alfam.nl>
2019-05-15 17:30:54 +0200

    Moved tracking the person to step argument transformation file

---
+++ UsingPronounsInSpecFlowScenarios/PersonStepArgumentTransformations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace UsingPronounsInSpecFlowScenarios
+{
+    [Binding]
+    class PersonStepArgumentTransformations
+    {
+        private Dictionary<string, Person> _persons = new Dictionary<string, Person>();
+
+        [StepArgumentTransformation("(.*)")]
+        public Person ConvertNameToPerson(string name)
+        {
+            if (!_persons.ContainsKey(name))
+            {
+                _persons.Add(name, new Person { Name = name });
+            }
+
+            return _persons[name];
+        }
+    }
+}
--- UsingPronounsInSpecFlowScenarios/PersonStepDefinitions.cs
+++ UsingPronounsInSpecFlowScenarios/PersonStepDefinitions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TechTalk.SpecFlow;
 
@@ -11,50 +7,35 @@ namespace UsingPronounsInSpecFlowScenarios
     [Binding]
     class PersonStepDefinitions
     {
-        private Dictionary<string, Person> _persons = new Dictionary<string, Person>();
-
         [Given(@"a man called '(.*)'")]
-        public void GivenAManCalled(string name)
+        public void GivenAManCalled(Person man)
         {
-            _persons.Add(name, new Person
-            {
-                Name = name,
-                Gender = Gender.Man
-            });
+            man.Gender = Gender.Man;
         }
 
         [Given(@"a woman called '(.*)'")]
-        public void GivenAWomanCalled(string name)
+        public void GivenAWomanCalled(Person woman)
         {
-            _persons.Add(name, new Person
-            {
-                Name = name,
-                Gender = Gender.Woman
-            });
+            woman.Gender = Gender.Woman;
         }
 
         [Given(@"'(.*)' lives at '(.*)'")]
-        public void GivenPersonLivesAt(string name, string address)
+        public void GivenPersonLivesAt(Person person, string address)
         {
-            var person = _persons[name];
             person.Address = address;
         }
 
         [When(@"'(.*)' and '(.*)' move in together at '(.*)'")]
-        public void WhenPerson1AndPerson2MoveInTogetherAt(string name1, string name2, string newAddress)
+        public void WhenPerson1AndPerson2MoveInTogetherAt(Person person1, Person person2, string newAddress)
         {
-            var person1 = _persons[name1];
-            var person2 = _persons[name2];
-
             Person.MoveInTogether(person1, person2, newAddress);
         }
 
         [Then(@"'(.*)' his address is '(.*)'")]
         [Then(@"'(.*)' her address is '(.*)'")]
-        public void ThenHisAddressIs(string name, string expectedAddress)
+        public void ThenHisAddressIs(Person person, string expectedAddress)
         {
-            var person = _persons[name];
-            Assert.AreEqual(expectedAddress, person.Address, $"Unexpected address for {name}");
+            Assert.AreEqual(expectedAddress, person.Address, $"Unexpected address for {person.Name}");
         }
     }
 }
