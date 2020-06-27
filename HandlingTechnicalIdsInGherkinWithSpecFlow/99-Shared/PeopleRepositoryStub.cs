01fa087e908602970a2e5f7ce552dddeafaf3e43
Ronald Bosma <ronaldbosma@outlook.com>
2020-06-27 15:36:54 +0200

    Reorder methods in stub

--- HandlingTechnicalIdsInGherkinWithSpecFlow/99-Shared/PeopleRepositoryStub.cs
+++ HandlingTechnicalIdsInGherkinWithSpecFlow/99-Shared/PeopleRepositoryStub.cs
@@ -6,17 +6,17 @@ namespace HandlingTechnicalIdsInGherkinWithSpecFlow.Shared
     {
         private readonly IDictionary<int, Person> _people = new Dictionary<int, Person>();
 
+        public Person GetById(int id)
+        {
+            return _people[id];
+        }
+
         public void AddRange(IEnumerable<Person> people)
         {
             foreach (var person in people)
             {
                 _people.Add(person.Id, person);
             }
-        }
-
-        public Person GetById(int id)
-        {
-            return _people[id];
         }
     }
 }
