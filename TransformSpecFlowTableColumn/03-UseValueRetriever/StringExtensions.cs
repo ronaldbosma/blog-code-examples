82db47df7c0a1fad3f3fd63cc7a3308851bf1ca2
Ronald Bosma <ronaldb@infosupport.com>
2022-10-29 10:23:23 +0200

    Remove reference to Shared from project 3

---
+++ TransformSpecFlowTableColumn/03-UseValueRetriever/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TransformSpecFlowTableColumn.UseValueRetriever
+{
+    public static class StringExtensions
+    {
+        public static int LocationToId(this string location)
+        {
+            // The GetHashCode method generates a different id per test run execution.
+            // Because I've added the id in the Gherkin scenario it needs to be the same value every time.
+
+            switch (location)
+            {
+                case "Amsterdam": return 1;
+                case "London": return 2;
+                case "Madrid": return 3;
+                default: throw new ArgumentException("Unknown location", nameof(location));
+            }
+        }
+    }
+}
