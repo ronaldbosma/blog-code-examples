ea2777bbbe648b34f6eff048795ac9c56b8d6d62
Ronald Bosma <ronaldb@infosupport.com>
2022-10-29 10:15:40 +0200

    Remove location id mapping from init

--- TransformSpecFlowTableColumn/01-Init/StringExtensions.cs
+++ TransformSpecFlowTableColumn/01-Init/StringExtensions.cs
@@ -4,13 +4,7 @@
     {
         public static int LocationToId(this string location)
         {
-            switch (location)
-            {
-                case "Amsterdam": return 1;
-                case "London": return 2;
-                case "Madrid": return 3;
-                default: throw new ArgumentException("Unknown location", nameof(location));
-            }
+            return Math.Abs(location.GetHashCode());
         }
     }
 }
