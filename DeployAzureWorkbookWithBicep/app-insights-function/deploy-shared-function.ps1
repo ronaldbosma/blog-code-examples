794830a5af8c142f3ad85f375270f8fa68c4e3fd
Ronald Bosma <ronaldb@infosupport.com>
2023-02-26 14:08:27 +0100

    use temp folder for request body file

--- DeployAzureWorkbookWithBicep/app-insights-function/deploy-shared-function.ps1
+++ DeployAzureWorkbookWithBicep/app-insights-function/deploy-shared-function.ps1
@@ -28,18 +28,19 @@ if ($null -ne $function)
     az rest --method "DELETE" --url """https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/components/$AppInsightsName/analyticsItems/item?api-version=2015-05-01&includeContent=true&scope=shared&type=function&name=$FunctionName"""
 }
 
+
 Write-Host "Load $FunctionFilePath"
 $content = Get-Content -Path $FunctionFilePath
 $content = $content -Replace """", "\"""  # Escape " in the query
-
 foreach ($key in $Placeholders.Keys)
 {
     $content = $content -Replace "##$key##", $Placeholders[$key]
 }
 
-Set-Content -Path "./body.local.json" -Value @"
+
+$requestBodyPath = Join-Path $env:TEMP "create-shared-function-request-body.json"
+Set-Content -Path $requestBodyPath -Value @"
 {
-    "id": "bf1d67fa-d92d-4714-aa95-74b0dd2c9a0a",
     "scope": "shared",
     "type": "function",
     "name": "$FunctionName",
@@ -51,4 +52,4 @@ Set-Content -Path "./body.local.json" -Value @"
 "@
 
 Write-Host "Add $FunctionName function"
-az rest --method "PUT" --url "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/components/$AppInsightsName/analyticsItems/item?api-version=2015-05-01" --headers "Content-Type=application/json" --body '@body.local.json'
+az rest --method "PUT" --url "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/components/$AppInsightsName/analyticsItems/item?api-version=2015-05-01" --headers "Content-Type=application/json" --body "@$requestBodyPath"
