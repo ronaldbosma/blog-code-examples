78533c868dfb411c2779aea320e27719ab6ab48a
Ronald Bosma <ronaldb@infosupport.com>
2023-02-26 14:37:43 +0100

    Add comments

--- DeployAzureWorkbookAndAppInsightsFunction/app-insights-function/deploy-shared-function.ps1
+++ DeployAzureWorkbookAndAppInsightsFunction/app-insights-function/deploy-shared-function.ps1
@@ -1,27 +1,35 @@
 [CmdletBinding()]
 param(
+    # Azure Subscription Id
     [Parameter(Mandatory=$true)]
     [string]$SubscriptionId,
 
+    # Name of the resource group of the Application Insights resource
     [Parameter(Mandatory=$true)]
     [string]$ResourceGroup,
 
+    # Name of the Application Insights resource
     [Parameter(Mandatory=$true)]
     [string]$AppInsightsName,
 
+    # Name of the function
     [Parameter(Mandatory=$true)]
     [string]$FunctionName,
 
+    # Path to the file containing the function content
     [Parameter(Mandatory=$true)]
     [string]$FunctionFilePath,
 
+    # Hashtable of placeholders that should be replaced in the function content
     [Parameter()]
     [hashtable]$Placeholders
 )
 
+# Retrieve existing functions
 $functions = az rest --method get --url "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/components/$AppInsightsName/analyticsItems?api-version=2015-05-01"
 $function = $functions | ConvertFrom-Json | Where-Object -Property "name" -Value $FunctionName -EQ
 
+# Delete existing function if found
 if ($null -ne $function)
 {
     Write-Host "Delete $FunctionName function"
@@ -29,6 +37,7 @@ if ($null -ne $function)
 }
 
 
+# Load the function content
 Write-Host "Load $FunctionFilePath"
 $content = Get-Content -Path $FunctionFilePath
 $content = $content -Replace """", "\"""  # Escape " in the query
@@ -38,6 +47,7 @@ foreach ($key in $Placeholders.Keys)
 }
 
 
+# Create a request body file used to create the function
 $requestBodyPath = Join-Path $env:TEMP "create-shared-function-request-body.json"
 Set-Content -Path $requestBodyPath -Value @"
 {
@@ -51,5 +61,6 @@ Set-Content -Path $requestBodyPath -Value @"
 }
 "@
 
+# Create the function
 Write-Host "Add $FunctionName function"
 az rest --method "PUT" --url "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/components/$AppInsightsName/analyticsItems/item?api-version=2015-05-01" --headers "Content-Type=application/json" --body "@$requestBodyPath"
