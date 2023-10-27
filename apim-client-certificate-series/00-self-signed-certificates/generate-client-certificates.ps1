06390cd2000ad3bbd8e74f51f6b82b59d7eaca7e
Ronald Bosma <ronaldb@infosupport.com>
2023-10-27 11:05:39 +0200

    Export client certificates as .pfx and create an expired client cert

--- apim-client-certificate-series/00-self-signed-certificates/generate-client-certificates.ps1
+++ apim-client-certificate-series/00-self-signed-certificates/generate-client-certificates.ps1
@@ -8,6 +8,7 @@ Set-StrictMode -Version Latest
 
 $currentScriptPath = $MyInvocation.MyCommand.Path | Split-Path -Parent
 $exportPath = "$currentScriptPath/certificates"
+$certificatePassword = ConvertTo-SecureString -String 'P@ssw0rd' -Force -AsPlainText
 
 
 # =====================================================================
@@ -27,6 +28,8 @@ $rootCA = New-SelfSignedRootCACertificate -Subject "CN=APIM Sample Root CA"
 $devIntermediateCA = New-SelfSignedIntermediateCACertificate -Subject "CN=APIM Sample DEV Intermediate CA" -Signer $rootCA
 $devClient01 = New-SelfSignedClientCertificate -Subject "CN=Client 01" -DnsName "Client 01" -Signer $devIntermediateCA
 $devClient02 = New-SelfSignedClientCertificate -Subject "CN=Client 02" -DnsName "Client 02" -Signer $devIntermediateCA
+# create an expired client certificate for testing purposes
+$devExpiredClient = New-SelfSignedClientCertificate -Subject "CN=Expired Client" -DnsName "Expired Client" -Signer $devIntermediateCA -ExpiresInMonths 0
 
 # create certificates for tst environment
 $tstIntermediateCA = New-SelfSignedIntermediateCACertificate -Subject "CN=APIM Sample TST Intermediate CA" -Signer $rootCA
@@ -43,12 +46,24 @@ if (-not(Test-Path -Path $exportPath))
     New-Item -Path $exportPath -ItemType Directory | Out-Null
 }
 
+# Export the certificates without private key as base64 encoded X.509 (.cer) files
+
 Export-CertificateAsBase64 -Certificate $rootCA -OutputFilePath "$exportPath\root-ca.cer"
 
 Export-CertificateAsBase64 -Certificate $devIntermediateCA -OutputFilePath "$exportPath\dev-intermediate-ca.cer"
 Export-CertificateAsBase64 -Certificate $devClient01 -OutputFilePath "$exportPath\dev-client-01.cer"
 Export-CertificateAsBase64 -Certificate $devClient02 -OutputFilePath "$exportPath\dev-client-02.cer"
+Export-CertificateAsBase64 -Certificate $devExpiredClient -OutputFilePath "$exportPath\dev-expired-client.cer"
 
 Export-CertificateAsBase64 -Certificate $tstIntermediateCA -OutputFilePath "$exportPath\tst-intermediate-ca.cer"
 Export-CertificateAsBase64 -Certificate $tstClient01 -OutputFilePath "$exportPath\tst-client-01.cer"
 Export-CertificateAsBase64 -Certificate $tstClient02 -OutputFilePath "$exportPath\tst-client-02.cer"
+
+# Export the client certificates with private key as .pfx file
+
+Export-PfxCertificate -Cert $devClient01 -FilePath "$exportPath\dev-client-01.pfx" -Password $certificatePassword
+Export-PfxCertificate -Cert $devClient02 -FilePath "$exportPath\dev-client-02.pfx" -Password $certificatePassword
+Export-PfxCertificate -Cert $devExpiredClient -FilePath "$exportPath\dev-expired-client.pfx" -Password $certificatePassword
+
+Export-PfxCertificate -Cert $tstClient01 -FilePath "$exportPath\tst-client-01.pfx" -Password $certificatePassword
+Export-PfxCertificate -Cert $tstClient02 -FilePath "$exportPath\tst-client-02.pfx" -Password $certificatePassword
