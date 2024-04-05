aa419a6cfb706a480608903846ff247bcca1a4e9
Ronald Bosma <ronaldb@infosupport.com>
2024-04-05 13:05:05 +0200

    Add check on column name

--- TransformSpecFlowTableColumn/05-TransformColumn/TableExtensions.cs
+++ TransformSpecFlowTableColumn/05-TransformColumn/TableExtensions.cs
@@ -4,18 +4,28 @@ namespace TransformSpecFlowTableColumn.TransformColumn
 {
     internal static class TableExtensions
     {
+        /// <summary>
+        /// Transforms the Location column with location names into a LocationId column with location ids.
+        /// </summary>
         public static Table TransformLocationNameToId(this Table table)
         {
-            return table.TransformColumn("Location", "LocationId", (s) => s.LocationToId().ToString());
+            return table.TransformColumn("Location", "LocationId", (locationName) => locationName.LocationToId().ToString());
         }
 
+        /// <summary>
+        /// Renames the column <paramref name="oldColumn"/> to <paramref name="newColum"/> 
+        /// and applies the <paramref name="transform"/> function to each value in the column.
+        /// </summary>
         public static Table TransformColumn(this Table table, string oldColumn, string newColum, Func<string, string> transform)
         {
-            table.RenameColumn(oldColumn, newColum);
-
-            foreach (var row in table.Rows)
+            if (table.ContainsColumn(oldColumn))
             {
-                row[newColum] = transform(row[newColum]);
+                table.RenameColumn(oldColumn, newColum);
+
+                foreach (var row in table.Rows)
+                {
+                    row[newColum] = transform(row[newColum]);
+                }
             }
 
             return table;
