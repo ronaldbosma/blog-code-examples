2c80d2b72c0feca3b20289e30f8a52cef085a6f5
Ronald Bosma <ronaldb@infosupport.com>
2024-08-10 10:03:40 +0200

    Add integration tests for all rules

--- validate-apim-policies-with-psrule/tests/APIMPolicy.Tests.ps1
+++ validate-apim-policies-with-psrule/tests/APIMPolicy.Tests.ps1
@@ -1,5 +1,5 @@
 <#
-    Integration tests that are executed on the src folder with sample policies.
+    Integration tests that are executed on the src folder with the sample policies.
 #>
 
 BeforeAll {
@@ -28,7 +28,6 @@ BeforeAll {
 
 Describe "APIMPolicy" {
     It "APIMPolicy.Rules.BackendBasePolicy" {
-
         $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.BackendBasePolicy' } );
         $ruleResults.Count | Should -Be 8
 
@@ -41,5 +40,85 @@ Describe "APIMPolicy" {
         Assert-RuleFailedForTarget $ruleResults "bad.product.cshtml"
         Assert-RuleFailedForTarget $ruleResults "bad.api.cshtml"
         Assert-RuleFailedForTarget $ruleResults "bad.operation.cshtml"
+    }
+    
+    It "APIMPolicy.Rules.BackendForwardRequestGlobalPolicy" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.BackendForwardRequestGlobalPolicy' } );
+        $ruleResults.Count | Should -Be 2
+
+        Assert-RuleSucceededForTarget $ruleResults "good\global.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad\global.cshtml"
+    }
+
+    It "APIMPolicy.Rules.InboundBasePolicy" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.InboundBasePolicy' } );
+        $ruleResults.Count | Should -Be 8
+
+        Assert-RuleSucceededForTarget $ruleResults "good.workspace.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.product.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.api.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.operation.cshtml"
+        
+        Assert-RuleFailedForTarget $ruleResults "bad.workspace.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.product.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.api.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.operation.cshtml"
+    }
+    
+    It "APIMPolicy.Rules.OnErrorBasePolicy" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.OnErrorBasePolicy' } );
+        $ruleResults.Count | Should -Be 8
+
+        Assert-RuleSucceededForTarget $ruleResults "good.workspace.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.product.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.api.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.operation.cshtml"
+        
+        Assert-RuleFailedForTarget $ruleResults "bad.workspace.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.product.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.api.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.operation.cshtml"
+    }
+    
+    It "APIMPolicy.Rules.OutboundBasePolicy" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.OutboundBasePolicy' } );
+        $ruleResults.Count | Should -Be 8
+
+        Assert-RuleSucceededForTarget $ruleResults "good.workspace.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.product.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.api.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.operation.cshtml"
+        
+        Assert-RuleFailedForTarget $ruleResults "bad.workspace.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.product.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.api.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.operation.cshtml"
+    }
+    
+    It "APIMPolicy.Rules.RemoveSubscriptionKeyHeader" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.RemoveSubscriptionKeyHeader' } );
+        $ruleResults.Count | Should -Be 2
+
+        Assert-RuleSucceededForTarget $ruleResults "good\global.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad\global.cshtml"
+    }
+    
+    It "APIMPolicy.Rules.UseBackendEntity" {
+        $ruleResults = @( $result | Where-Object { $_.RuleName -eq 'APIMPolicy.Rules.UseBackendEntity' } );
+        $ruleResults.Count | Should -Be 12
+
+        Assert-RuleSucceededForTarget $ruleResults "good\global.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.workspace.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.product.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.api.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.operation.cshtml"
+        Assert-RuleSucceededForTarget $ruleResults "good.fragment.cshtml"
+        
+        Assert-RuleFailedForTarget $ruleResults "bad\global.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.workspace.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.product.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.api.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.operation.cshtml"
+        Assert-RuleFailedForTarget $ruleResults "bad.fragment.cshtml"
     }
 }
