a1a8379cae368dbf4bb5dcc5c3b9ecfbe620180d
Ronald Bosma <ronaldb@infosupport.com>
2024-08-12 11:41:25 +0200

    Refactor convention to loop over array with foreach keyword

--- validate-apim-policies-with-psrule/.ps-rule/APIM.Policy.Conventions.Rule.ps1
+++ validate-apim-policies-with-psrule/.ps-rule/APIM.Policy.Conventions.Rule.ps1
@@ -6,31 +6,34 @@
 
 # Synopsis: Imports the APIM XML policy file for analysis. File names should match: *.cshtml
 Export-PSRuleConvention "APIM.Policy.Conventions.Import" -Initialize {
-    $policies = @(Get-ChildItem -Path "." -Include "*.cshtml" -Recurse -File | ForEach-Object {
 
+    $policies = @()
+    $policyFiles = Get-ChildItem -Path "." -Include "*.cshtml" -Recurse -File
+
+    foreach ($policyFile in $policyFiles) {
         # Use the relative path of the file as the object name, this makes it easier to e.g. apply suppressions.
         # Also replace backslashes with forward slashes, so the path doesn't differ between Windows and Linux.
         # Example: ./src/my.api.cshtml
-        $name = ($_.FullName | Resolve-Path -Relative).Replace('\', '/')
+        $name = ($policyFile.FullName | Resolve-Path -Relative).Replace('\', '/')
 
         # Determine the level of the policy based on the file name
         $level = $null
-        if ($_.Name -eq "global.cshtml") { $level = "Global" }
-        elseif ($_.Name.EndsWith(".workspace.cshtml")) { $level = "Workspace" }
-        elseif ($_.Name.EndsWith(".product.cshtml")) { $level = "Product" }
-        elseif ($_.Name.EndsWith(".api.cshtml")) { $level = "API" }
-        elseif ($_.Name.EndsWith(".operation.cshtml")) { $level = "Operation" }
-        elseif ($_.Name.EndsWith(".fragment.cshtml")) { $level = "Fragment" }
+        if ($policyFile.Name -eq "global.cshtml") { $level = "Global" }
+        elseif ($policyFile.Name.EndsWith(".workspace.cshtml")) { $level = "Workspace" }
+        elseif ($policyFile.Name.EndsWith(".product.cshtml")) { $level = "Product" }
+        elseif ($policyFile.Name.EndsWith(".api.cshtml")) { $level = "API" }
+        elseif ($policyFile.Name.EndsWith(".operation.cshtml")) { $level = "Operation" }
+        elseif ($policyFile.Name.EndsWith(".fragment.cshtml")) { $level = "Fragment" }
 
-        # Only create an object to analyse if the level is known
+        # Only create a policy object to analyse if the level is known
         if ($null -ne $level) {
-            [PSCustomObject]@{
+            $policies += [PSCustomObject]@{
                 Name = $name
                 Level = $level
-                Content = [Xml](Get-Content -Path $_.FullName -Raw)
+                Content = [Xml](Get-Content -Path $policyFile.FullName -Raw)
             }
         }
-    })
+    }
 
     $PSRule.ImportWithType("APIM.Policy", $policies);
 }
