4e50f23e4e1d8427d2246ce563f7ae38268dfdf5
Ronald Bosma <ronaldb@infosupport.com>
2024-08-21 16:34:05 +0200

    Add comments

--- validate-apim-policies-with-psrule/tests/Invoke-PesterTests.ps1
+++ validate-apim-policies-with-psrule/tests/Invoke-PesterTests.ps1
@@ -4,21 +4,26 @@
 #>
 
 param (
+    # Path to the folder containing the Pester test files. Will search recursively for files with the pattern "*Tests.ps1".
     [string]
-    $ModulePath = ".",
+    $Path = ".",
 
+    # Filter to include only specific test files. For example: "*InboundBasePolicy*"
     [string]
     $IncludeTestFiles = "*",
 
+    # Will enable test and code coverage results to be published
     [Parameter(Mandatory = $false)]
     [switch]
     $Publish,
 
+    # Path where the test and code coverage results will be published
     [Parameter(Mandatory=$false)]
     [string]
     $ResultsPath
 )
 
+# Install the Pester PowerShell module if it is not available
 $pesterModule = Get-Module -Name Pester -ListAvailable | Where-Object {$_.Version -like '5.*'}
 if (!$pesterModule) { 
     try {
@@ -30,17 +35,21 @@ if (!$pesterModule) {
     }
 }
 
+# Import the Pester module
 Write-Host "Pester version: $($pesterModule.Version.Major).$($pesterModule.Version.Minor).$($pesterModule.Version.Build)"
 $pesterModule | Import-Module
 
+# Create results path directory if results should be published and the path does not exist
 if ($Publish) {
     if (!(Test-Path -Path $ResultsPath)) {
         New-Item -Path $ResultsPath -ItemType Directory -Force | Out-Null
     }
 }
 
+# Search for test files
 $tests = (Get-ChildItem -Path $($ModulePath) -Include $IncludeTestFiles -Recurse | Where-Object {$_.Name -like "*.Tests.ps1"}).FullName
 
+# Execute Pester tests
 if ($Publish) {
     $files = (Get-ChildItem -Recurse | Where-Object {$_.Name -like "*.psm1" -or $_.Name -like "*.ps1" -and $_.FullName -notlike "*\Pipelines\*"}).FullName
     Invoke-Pester $tests -OutputFile "$ResultsPath\Test-Pester.xml" -OutputFormat NUnitXml -CodeCoverage $($files.FullName) -CodeCoverageOutputFile "$($ResultsPath)\Pester-Coverage.xml" -CodeCoverageOutputFileFormat JaCoCo
