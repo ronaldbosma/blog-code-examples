9ec00180c04757b2de0bb65625002754c39f1b6d
Ronald Bosma <ronaldb@infosupport.com>
2024-08-21 17:34:08 +0200

    Use pester configuration when running tests Several of the used parameters were deprecated.

--- validate-apim-policies-with-psrule/tests/Invoke-PesterTests.ps1
+++ validate-apim-policies-with-psrule/tests/Invoke-PesterTests.ps1
@@ -1,26 +1,17 @@
 <#
     Script to execute Pester tests.
-    Source: https://www.logitblog.com/increase-the-success-rate-of-azure-devops-pipelines-using-pester/
+    Based on: https://www.logitblog.com/increase-the-success-rate-of-azure-devops-pipelines-using-pester/
 #>
 
 param (
-    # Path to the folder containing the Pester test files. Will search recursively for files with the pattern "*Tests.ps1".
+    # Path to the folder containing the Pester test files. Default is the current folder.
     [string]
     $Path = ".",
 
-    # Filter to include only specific test files. For example: "*InboundBasePolicy*"
-    [string]
-    $IncludeTestFiles = "*",
-
-    # Will enable test and code coverage results to be published
-    [Parameter(Mandatory = $false)]
-    [switch]
-    $Publish,
-
-    # Path where the test and code coverage results will be published
+    # Path where the test results will be published. Default is not to publish test results.
     [Parameter(Mandatory=$false)]
     [string]
-    $ResultsPath
+    $TestResultsPath
 )
 
 # Install the Pester PowerShell module if it is not available
@@ -39,20 +30,15 @@ if (!$pesterModule) {
 Write-Host "Pester version: $($pesterModule.Version.Major).$($pesterModule.Version.Minor).$($pesterModule.Version.Build)"
 $pesterModule | Import-Module
 
-# Create results path directory if results should be published and the path does not exist
-if ($Publish) {
-    if (!(Test-Path -Path $ResultsPath)) {
-        New-Item -Path $ResultsPath -ItemType Directory -Force | Out-Null
-    }
+
+$config = New-PesterConfiguration
+$config.Run.Path = $Path
+
+# Enable test result generation if the TestResultsPath parameter is provided
+if (-not([string]::IsNullOrWhiteSpace($TestResultsPath))) {
+    $config.TestResult.OutputFormat = "NUnitXml"
+    $config.TestResult.OutputPath = TestResultsPath
+    $config.TestResult.Enabled = $True
 }
 
-# Search for test files
-$tests = (Get-ChildItem -Path $($ModulePath) -Include $IncludeTestFiles -Recurse | Where-Object {$_.Name -like "*.Tests.ps1"}).FullName
-
-# Execute Pester tests
-if ($Publish) {
-    $files = (Get-ChildItem -Recurse | Where-Object {$_.Name -like "*.psm1" -or $_.Name -like "*.ps1" -and $_.FullName -notlike "*\Pipelines\*"}).FullName
-    Invoke-Pester $tests -OutputFile "$ResultsPath\Test-Pester.xml" -OutputFormat NUnitXml -CodeCoverage $($files.FullName) -CodeCoverageOutputFile "$($ResultsPath)\Pester-Coverage.xml" -CodeCoverageOutputFileFormat JaCoCo
-} else {
-    Invoke-Pester $tests
-}
+Invoke-Pester -Configuration $config
